11f55871a4175a22f3d50edbb126b2627fb3295c
onavarrete04 <mafismafis04@gmail.com>
2021-11-24 12:02:08 -0500

    abriendo, leyendo, y agregando contenido a archivos de texto, se utiliza la funcion open que permite agregar dos parametros, el primero es el nombre del texto, y la segunda la funcion, a se para añadir elementos sin borrar los ya existentes en el archivo, w se utiliza cuando creamos el archivo para escribir y sepa que es de texto, y r, es para leer, siempre se debe cerrar el archivo apenas hayamos cumplido la tarea, para liberar el documento si es que se requiere para otro programa, y es una tarea que no debe olvidarse.

---
+++ abrirarchivos.py
@@ -0,0 +1,74 @@
+# una actividad de un programa es el almacenañamiento y recuperación de datos almacenados
+
+# modos de almacenar (archivos de texto, binarios, bases de datos, etc)
+
+archivo_1 = open("datos.txt","w") # 
+# para crear el archivo se utiliza la función open
+# se pasan dos parametros, el primero es el nombre del archivo
+# el segundo es "w" indica que se crea un archivo de texto
+# si haya un archivo con el mismo nombre, se crea uno nuevo y se borra el otro
+
+archivo_1.write("Primera línea. \n")
+archivo_1.write("Segunda línea. \n")
+archivo_1.write("Tercera línea. \n")
+archivo_1.close() # se cierra obligatoriamente para que otro programa interactue con el archivo
+# es una tarea que no se debe olvidar
+
+# leer el archivo
+
+archivo_1 = open("datos.txt","r") # read
+contenido = archivo_1.read() # en la función read
+# podemos pasar un entero con la cantidad de texto (str) que queremos leer, 
+# si le pasamos un 1, va imprimir una P de primera linea, si pasamos un 6, casi la cadena de primera
+# si lo dejamos vacio muestra todas los caracteres  y string dentro del documento
+
+
+print(contenido)
+archivo_1.close()
+
+# leer el archivo linea a linea
+
+
+
+archivo_1 = open("datos.txt","r")
+linea = archivo_1.readline() # se lee la primera linea del archivo
+while linea != "":
+    print(linea, end="")
+    linea = archivo_1.readline() # se lee la linea segunda, y se va actualizando
+
+archivo_1.close()
+
+# guardar lineas de texto en una lista
+
+
+archivo_1 = open("datos.txt","r") # se abre el archivo
+linea_1 = archivo_1.readlines() # la función readlineas crea una lista
+
+print("El archivo tiene", len(linea_1), "lineas") # como ya esta creada la lista, con la función len
+# se cuentan los elementos de la lista - especificamente cada linea
+print("contenido del archivo")
+
+for i in linea_1: # se itera dentro de la lista, accediendo a cada elemento de la lista
+    # la i se convierte en el index que lama a cada elemento de la lista
+    print(i, end="")
+print(linea_1) # se hace la prueba de impresión de la lista completa
+archivo_1.close()
+
+
+# ABRIR ARCHIVO Y AGREGAR LINEAS
+
+# para abrir el archivo sin que se borren las lineas ya existentes
+# se abre con la "a" de append
+
+archivo_1 = open("datos.txt","a")
+archivo_1.write("nueva linea 1 ejemplo \n")
+archivo_1.write("nueva linea 2 ejemplo \n")
+archivo_1.write("nueva linea 3 ejemplo \n")
+archivo_1.close()
+
+archivo_1 = open("datos.txt","r")
+leer = archivo_1.read() # hay que crear un objeto que lea el contenido
+print(leer)
+archivo_1.close()
+
+
